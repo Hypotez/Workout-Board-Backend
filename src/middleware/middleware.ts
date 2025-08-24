61ee6eb9d3ccdf2695f4d5ae3aa6cdedc131d087
Aleksander Odland Fattnes <Aleksander.odland@lyse.net>
2025-08-24 17:06:39 +0200

    feat: Changed types

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,13 +1,13 @@
 import { NextFunction, Request, Response } from 'express'
-import { ErrorResponse, SuccessResponse, SuccessStatusCode, ErrorStatusCode,  Error } from '../types';
+import { ErrorResponse, SuccessResponse, SuccessStatusCode, ErrorStatusCode,  Error, successString, errorString } from '../types';
 
 export function apiResponseMiddleware(req: Request, res: Response, next: NextFunction) {
   res.success = function<T> (data: T, statusCode: SuccessStatusCode = 200) {
-    return res.status(statusCode).json({ status: "success", data: data } as SuccessResponse<T>);
+    return res.status(statusCode).json({ status: successString, data: data } as SuccessResponse<T>);
   };
 
   res.error = function (err: Error = null, statusCode: ErrorStatusCode = 400) {
-    return res.status(statusCode).json({ status: "error", error: err } as ErrorResponse);
+    return res.status(statusCode).json({ status: errorString, error: err } as ErrorResponse);
   };
 
   next();
--- src/types.ts
+++ src/types.ts
@@ -1,36 +1,48 @@
 import { z } from "zod";
 
+export const successString = "success"
+export const errorString = "error"
+
 const SuccessStatusCodeSchema = z.literal(200);
 const ErrorStatusCodeSchema = z.union([z.literal(400), z.literal(401), z.literal(403), z.literal(404), z.literal(500)]);
 const ErrorSchema = z.string().nullable();
 const DataSchema = z.unknown();
+const SuccessStatusSchema = z.literal(successString)
+const ErrorStatusSchema = z.literal(errorString)
+
 export const urlSchema = z.url("Not a valid URL").brand<"Url">();
 export const uuidSchema = z.uuid("Not a valid UUID").brand<"Uuid">();
 export const nonEmptyStringSchema = z.string().min(1, "String cannot be empty").brand<"NonEmptyString">();
 
 export type SuccessStatusCode = z.infer<typeof SuccessStatusCodeSchema>;
 export type ErrorStatusCode = z.infer<typeof ErrorStatusCodeSchema>;
+export type SuccessStatus = z.infer<typeof SuccessStatusSchema>;
+export type ErrorStatus = z.infer<typeof ErrorStatusSchema>;
 export type Url = z.infer<typeof urlSchema>;
 export type Uuid = z.infer<typeof uuidSchema>;
 export type Error = z.infer<typeof ErrorSchema>;
+export type Data = z.infer<typeof DataSchema>;
 
-export const SuccessResponseSchema = <T extends z.ZodTypeAny>(dataSchema: T) =>
+export const SuccessResponseSchema =
   z.object({
-    status: z.literal("success"),
-    data: dataSchema,
+    status: SuccessStatusSchema,
+    data: DataSchema,
   });
 
-export type ErrorResponse = {
-    status: "error";
-    error: Error;
-}
+export const ErrorResponseSchema = z.object({
+  status: ErrorStatusSchema,
+  error: ErrorSchema,
+});
 
-export type SuccessResponse<T = unknown> = {
-  status: "success";
-  data: T;
+export type SuccessResponse<Data> = {
+  status: SuccessStatus;
+  data: Data;
 };
 
-export type ApiResponse<T = unknown> = SuccessResponse<T> | ErrorResponse;
+export type ErrorResponse = {
+  status: ErrorStatus;
+  error: Error;
+};
 
 export interface ResponseHelpers {
   success: <T = unknown>(
@@ -43,8 +55,6 @@ export interface ResponseHelpers {
     statusCode?: ErrorStatusCode
   ) => void;
 }
-
-
 
 export interface Service {
     hevyClient: HevyClientService;
