e6420e73f70d5e1b62cdeba3917a48b014f6f453
Aleksander Odland Fattnes <Aleksander.odland@lyse.net>
2025-10-10 00:09:59 +0200

    feat: Added cookie authentication middleware

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,6 +1,43 @@
 import { NextFunction, Request, Response } from 'express'
 import { ErrorResponse, ErrorStatusCode, ErrorString, ErrorType, SuccessResponse, SuccessStatusCode, SuccessString } from '../schemas/shared/api';
+import { clearCookies, generateTokens, getPayload, setCookies, verifyAccessToken, verifyRefreshToken } from '../crypto/jwt';
 
+function invalidAuth(res: Response) {
+    clearCookies(res)
+    return res.error('Please login', 401)
+}
+
+export async function cookieAuth(req: Request, res: Response, next: NextFunction) {
+  const accessToken = req.cookies.access_token
+  const refreshToken = req.cookies.refresh_token
+
+  if (!accessToken) {
+    if (!refreshToken) {
+      return invalidAuth(res)
+    }
+
+    const refreshTokenValid = verifyRefreshToken(refreshToken)
+    if (!refreshTokenValid) {
+      return invalidAuth(res)
+    }
+
+    const payload = getPayload(refreshToken)
+    if (!payload) {
+      return invalidAuth(res)
+    }
+
+    const tokensInformation = generateTokens(payload.id)
+    await setCookies(res, tokensInformation)
+    return next()
+  }
+
+  const accessTokenValid = verifyAccessToken(accessToken)
+  if (!accessTokenValid) {
+    return invalidAuth(res)
+  }
+
+  next()
+}
 
 export function apiResponseMiddleware(req: Request, res: Response, next: NextFunction) {
   res.success = function<T> (data: T, statusCode: SuccessStatusCode = 200) {
