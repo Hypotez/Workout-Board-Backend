c80f63c2f32fe1fe316f853fc5238f8078144173
Aleksander Odland Fattnes <Aleksander.odland@lyse.net>
2025-10-10 00:28:02 +0200

    feat: Added auth schemas and auth endpoints

---
+++ src/routes/auth.ts
@@ -0,0 +1,57 @@
+import { setCookies, clearCookies } from '../crypto/jwt.js'
+import { CreateUserSchema, LoginUserSchema } from '../schemas/shared/auth.js'
+import { cookieAuth } from '../middleware/middleware.js'
+import { Router } from 'express'
+
+
+const router = Router()
+
+router.post('/register', async (req, res): Promise<void> => {
+    const user = CreateUserSchema.safeParse(req.body)
+
+    if (!user.success) {
+        res.error('Invalid data')
+        return
+    }
+
+    /*
+    TODO: Enable user registration
+    const cookie = await req.service.db.createUser({ username: user.data.username, password: user.data.password, email: user.data.email })
+    if (cookie) {
+        await setCookies(res, cookie)
+        res.success()
+        return
+    }
+    */
+    res.error('Could not create user', 400)
+})
+
+router.post('/login', async (req, res): Promise<void> => {
+    const login = LoginUserSchema.safeParse(req.body)
+
+    if (!login.success) {
+        res.error('Invalid data')
+        return
+    }
+
+    /*
+    TODO: Enable user login
+    const cookie = await req.service.db.login(login.data)
+    if (cookie) {
+        await setCookies(res, cookie)
+        res.success()
+        return
+    }
+    */
+
+    res.error('Invalid username, email or password', 401)
+})
+
+router.post('/logout', async (_, res): Promise<void> => {
+    clearCookies(res)
+    return res.success()
+})
+
+router.get('/validate', cookieAuth, async (_, res): Promise<void> => {
+    return res.success()
+})
--- src/schemas/shared/auth.ts
+++ src/schemas/shared/auth.ts
@@ -1,9 +1,35 @@
 import { z } from "zod";
 
+const usernameField = z.string().min(1, 'Username must be a non-empty string');
+const passwordField = z.string().min(1, 'Password must be a non-empty string');
+const emailField = z.email('Invalid email format');
+
 export const CookieResponseSchema = z.object({
   access_token: z.string(),
   refresh_token: z.string(),
   access_token_expiration: z.number(),
   refresh_token_expiration: z.number()
 })
+
+export const CreateUserSchema = z.object({
+  username: usernameField,
+  password: passwordField,
+  email: emailField
+})
+
+const LoginUsernameSchema = z.object({
+  type: z.literal('username'),
+  username: usernameField,
+  password: passwordField
+})
+
+const LoginEmailSchema = z.object({
+  type: z.literal('email'),
+  email: emailField,
+  password: passwordField
+})
+
+export const LoginUserSchema = z.discriminatedUnion('type', [LoginUsernameSchema, LoginEmailSchema])
+
+export type CreateUserInput = z.infer<typeof CreateUserSchema>
 export type CookieResponse = z.infer<typeof CookieResponseSchema>
