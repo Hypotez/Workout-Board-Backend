e72ac574096083fce6ccf2ad03d36893a161b6f8
Aleksander Odland Fattnes <Aleksander.odland@lyse.net>
2025-11-02 00:09:30 +0100

    feat: Added error if not found by id

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -15,6 +15,10 @@ router.get('/me', attachUserId, async (req, res): Promise<void> => {
   }
 
   const user = await req.service.db.getUserById(userId);
+  if (!user) {
+    return res.error('User not found', 404);
+  }
+
   res.success(user);
 });
 
